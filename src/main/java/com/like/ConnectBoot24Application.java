799e56aafe051948210a805d7ab17d506c1bb328
devkbm <devkbm0417@gmail.com>
2021-04-21 23:04:35 +0900

    first commit

---
+++ src/main/java/com/like/ConnectBoot24Application.java
@@ -0,0 +1,13 @@
+package com.like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectBoot24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectBoot24Application.class, args);
+	}
+
+}
